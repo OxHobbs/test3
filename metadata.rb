d91e34966feb1f00f1929ff7e7fe2ba6fa159e93
azure user <azure@me.me>
2017-11-20 21:52:00 +0000

    updated depends

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,7 @@ description 'Installs/Configures test3'
 long_description 'Installs/Configures test3'
 version '0.1.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
+depends 'test2'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
